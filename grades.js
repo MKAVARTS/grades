6a378e86fa2500fab7065161fe07a0e26840252c
Mike Kluge <mkavarts@gmail.com>
2018-01-21 18:18:42 -0600

    updated

--- grades.js
+++ grades.js
@@ -1,3 +1,6 @@
+// create [scores] array that holds all of the grades
+// create a {grades} object will hold property (grade) a value (how many of each)
+
 const scores = [82, 71, 62, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87, 60];
 const grades = {
     A: 0,
@@ -5,9 +8,24 @@ const grades = {
     C: 0,
     D: 0,
     F: 0,
-}; // You'll need to change this line of code
+};
+
+// create <div> in HTML document for printing javascript loops 
 
 var gradesDiv = document.getElementById('grades');
+var mostCommonDiv = document.getElementById('most-common');
+var lowestCommonDiv = document.getElementById('lowest-common');
+
+// variables for grades
+
+var leastCommon = mostCommon;
+var leastCommonGrade;
+var mostCommon = 0;
+var mostCommonGrade;
+
+// subject each number in [scores] array to greater than equal to test at
+// the different grade values. If they meet the condition the loop increments
+// that grade letter's number in the {grades} object
 
 
 for (var i = 0; i < scores.length; i++) {
@@ -24,43 +42,67 @@ for (var i = 0; i < scores.length; i++) {
     }
 }
 
+// loop through {grades object} for the length of object
+//  and print to "grades" <div> each key (grade letter) 
+// and property (how many of each grade) to the HTML document. 
+
 for(key in grades) {
     gradesDiv.innerHTML += [key] + ' : ' + grades[key] + '<br>';
 }
+
+// sort [scores] array from highest to lowest using sort function 
 
 scores.sort(function(a,b) {
     return a - b;
 });
 
+// print lowest score (first score in in [score] array
+// and highest score (last score in [score] array) to HTML document.
+
 gradesDiv.innerHTML += '<br>'+ "lowest grade: " + scores[0] + '<br>';
 gradesDiv.innerHTML += '<br>'+ "higest grade: " + scores[13] + '<br>';
 
-var mostCommon = 0;
-var mostCommonGrade;
+
+// loop through {grades} object and test current position 
+// to the last position for a greater than or equal to condition.
+// at the end of the loop, store the highest number in most mostCommonGrade variable. 
 
 for(key in grades) {
-    if(grades[key] >= mostCommon) {
+    if(grades[key] >= grades[key]) {
         mostCommon = grades[key];
-    }
-    if(grades[key] === mostCommon) {
-        mostCommonGrade = key;
     }
 }
 
-gradesDiv.innerHTML += '<br>' + 'most common grade is: ' + mostCommonGrade +'<br>';
+// loop through {grades} object 
+// if "mostCommon" variable matches the letter grade
+// print the letter grade to HTML <div>
 
-var leastCommon
-var leastCommonGrade;
+for(key in grades) {
+    if(grades[key] === mostCommon) {
+    mostCommonGrade = key;
+    mostCommonDiv.innerHTML += mostCommonGrade + " ";
+  }
+}
 
+
+// loop through {grades} object and test current position 
+// to the previous condition for a less than or equal to condition
+// store the lowest grade in leastCommon varaible
 
 for(key in grades) {
     if(grades[key] <= grades[key]) {
-        grades[key] = grades[key];
         leastCommon = grades[key];
-    }
-    if(grades[key] === leastCommon) {
-        leastCommonGrade = key;
     }
 }
 
-gradesDiv.innerHTML += '<br>' + 'least common grade is: ' + leastCommonGrade;
+
+// loop through {grades} object 
+// if "leastCommon" variable matches the letter grade
+// print the letter grade to HTML <div>
+
+for(key in grades) {
+    if(grades[key] === leastCommon) {
+        leastCommonGrade = key;
+        lowestCommonDiv.innerHTML += leastCommonGrade + " ";
+  }
+}
